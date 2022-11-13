fa7c854f37786fed23b47d85ec6669b96a35223b
Gino_M <71620924+GinoGin@users.noreply.github.com>
2022-11-13 14:23:42 +0530

    Delete Project Development Phase/Sprint 1/server/Back End directory

--- Project Development Phase/Sprint 1/server/Back End/db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-CONNECTION_STRING = "mongodb+srv://*********@cluster0.zptfbvm.mongodb.net/test"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('heart_disease_prediction')
-users = pymongo.collection.Collection(db, 'Users')
